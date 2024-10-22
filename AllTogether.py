44d9ff50213166f9c07d654ba5e7a8bd5b75507b
Cruisenstein <Cruisenstein00@gmail.com>
2024-10-22 19:13:29 +0100

    done

--- AllTogether.py
+++ AllTogether.py
@@ -3,22 +3,10 @@
 import random
 list = random.sample(range(1, 100), 10)
 result = []
-result2 = []
-print(list)
-
-for i in (list):
-        if i % 2 != 0: 
-            result.append(i)
-    
-print(result) 
-
-# EVERYTHING ABOVE WORKS, JUST DOESN'T USE A WHILE LOOP#
-
 
 for n in list:
     k = 1
-    while k < 2:
-         if n % 2 != 0:
-              result2.append(n)
-              k += 1
-print(result2)
+    while n % 2 != 0:
+        result.append(n)
+        break
+print(result)
