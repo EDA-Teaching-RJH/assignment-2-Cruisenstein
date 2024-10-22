77faf2769af9b9729a6769717aea866dada86c64
Cruisenstein <Cruisenstein00@gmail.com>
2024-10-22 19:08:11 +0100

    annoyed

--- AllTogether.py
+++ AllTogether.py
@@ -2,9 +2,23 @@
 
 import random
 list = random.sample(range(1, 100), 10)
+result = []
+result2 = []
+print(list)
 
 for i in (list):
-    while i % 2:
-        list.pop(0)
+        if i % 2 != 0: 
+            result.append(i)
     
-print(list)
+print(result) 
+
+# EVERYTHING ABOVE WORKS, JUST DOESN'T USE A WHILE LOOP#
+
+
+for n in list:
+    k = 1
+    while k < 2:
+         if n % 2 != 0:
+              result2.append(n)
+              k += 1
+print(result2)
--- ListManipulation.py
+++ ListManipulation.py
@@ -2,7 +2,7 @@
 
 num = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 num.sort()
-num.extend("7" + '8')
+num.extend('7' + '8')
 num.remove(1)
 num.remove(1)
 print(num)
