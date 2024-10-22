b95beea152d4f293378c8d36513854b2a501f17c
Cruisenstein <Cruisenstein00@gmail.com>
2024-10-22 16:15:38 +0100

    done

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,15 @@
 ### Part Two -- your code goes here. 
+
+num = 34
+
+start = str(input("Would you like to begin? (y/n)"))
+
+while start == "y":
+    guess = int(input("Guess the number between 1-100."))
+    if guess == num:
+        print("WELL DONE CORRECT!")
+        break
+    elif guess < num:
+        print("HIGHER")
+    else:
+        print("LOWER")
